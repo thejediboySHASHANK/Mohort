7ec0a2d4530101a9bcd2aba8e7149f292aa6f944
Shashank Dewangan <shasnk1104@gmail.com>
2023-10-02 23:15:20 +0530

    Navbar updation v1

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+const Navbar = () => {
+    return (
+        <header className="w-full">
+            <nav className="nav">
+                <Link href="/" className="flex items-center gap-1">
+                    <Image
+                        src="/assets/icons/logo.svg"
+                        width={27}
+                        height={27}
+                        alt="logo"
+                    />
+
+                    <p className="nav-logo">
+                        Mohort - <span className="text-primary">Price Tracker</span>
+                    </p>
+                </Link>
+            </nav>
+        </header>
+    )
+}
+export default Navbar
